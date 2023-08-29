cc9f8aaa93fcc17743471f5fbc58f47163415e2c
Yago <yago.pereira@estudante.ufjf.br>
2023-08-28 22:41:02 -0300

    Avançando na criação da playlist

--- main.py
+++ main.py
@@ -8,17 +8,31 @@ vingadores = Filme('vingadores: guerra infinita', 2018, 160)
 
 atlanta = Serie('atlanta', 2018, 2)
 
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+
+demolidor = Serie('Demolidor', 2016, 2)
+
 # Em cada um dos objetos, atribuímos 5 likes
 
 for i in range(0, 5):
     vingadores.dar_like()
     atlanta.dar_like()
+    tmep.dar_like()
+    demolidor.dar_like()
 
 # Organizamos os objetos em uma lista
 
-series_e_filmes = [vingadores, atlanta]
+series_e_filmes = [vingadores, atlanta, tmep, demolidor]
+
+# criamos o objeto Playlist
+
+playlist_fim_de_semana = Playlist('fim de semana', series_e_filmes)
 
 # Exibimos as representações textuais dos objetos
 
-for programa in series_e_filmes:
+for programa in playlist_fim_de_semana.listagem:
     print(programa)
+
+# Imprimindo o tamanho da Playlist
+
+print(f'Tamanho da Playlist: {len(playlist_fim_de_semana.listagem)}')
--- modelo.py
+++ modelo.py
@@ -43,3 +43,16 @@ class Serie(Programa):
 
     def __str__(self):
         return f'{self.nome} - {self.ano} - {self.temporadas} temporadas - {self.likes} Likes'
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    @property
+    def listagem(self):
+        return self._programas
+    
+    @property
+    def tamanho(self):
+        return len(self._programas)

5e52a2ab1e03023a436120d1da2e60df2a020874
Yago <yago.pereira@estudante.ufjf.br>
2023-08-28 21:30:17 -0300

    Renomeando arquivos.

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 #!/bin/python3
 
-from Modelo import *
+from modelo import *
+
+# Criamos a seguir um objeto filme e um objeto série
 
 vingadores = Filme('vingadores: guerra infinita', 2018, 160)
 
 atlanta = Serie('atlanta', 2018, 2)
 
-series_e_filmes = [vingadores, atlanta]
+# Em cada um dos objetos, atribuímos 5 likes
 
 for i in range(0, 5):
     vingadores.dar_like()
     atlanta.dar_like()
+
+# Organizamos os objetos em uma lista
+
+series_e_filmes = [vingadores, atlanta]
+
+# Exibimos as representações textuais dos objetos
 
 for programa in series_e_filmes:
     print(programa)
